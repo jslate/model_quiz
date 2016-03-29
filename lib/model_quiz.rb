d2e2f70d2f728b9b50bf97ac38fa785a48699406
Jonathan Slate <jslate@patientslikeme.com>
2016-03-28 20:46:29 -0400

    actually quiz the user

--- lib/model_quiz.rb
+++ lib/model_quiz.rb
@@ -1,10 +1,40 @@
 require "model_quiz/version"
-require 'pry'
+require 'find'
 
 module ModelQuiz
 
   def self.quiz_me
-    puts `pwd`
+    hash = {}
+
+    Find.find('app/models') do |path|
+      next unless path.end_with?('.rb')
+
+      key = ''
+      File.readlines(path).each do |line|
+        line.match(/class (\S*) \< ActiveRecord::Base/) do |matches|
+          key = matches[1] if matches[1]
+        end
+
+        if key
+          line.match(/\A\s*(belongs_to|has_many|has_one)\s+:(\w*)/) do |matches|
+            hash[key] ||= []
+            hash[key] << matches.to_a.slice(1..-1)
+          end
+        end
+      end
+    end
+
+    while true
+      model = hash.keys.shuffle.last
+      relationship, attribute = hash[model].shuffle.last
+      attribute.gsub! /s\Z/, ''
+      puts '#' * 50
+      puts "#{model} _________________ #{attribute}"
+      STDIN.gets
+      puts relationship
+      STDIN.gets
+    end
+
   end
 
 end
